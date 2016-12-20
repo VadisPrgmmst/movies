90083b8455ac589e0bec1ccdb7266b1f2f0f9f70
vadis <vadis.programmist@mail.ru>
2016-12-20 22:50:32 +0300

    Search the title by using ARGV

--- file_reader.rb
+++ file_reader.rb
@@ -1,5 +1,7 @@
 #encoding: UTF-8
 
+
+
 path = File.dirname(__FILE__) + './lesson2/movies.txt'
 
 if File.exist? path
@@ -10,10 +12,21 @@ else
 end
 
 File.open(path) do |file|
+
+  found_title = nil
+
   file.readlines.each do |line|
     title = line.split('|')[1]
     rating = line.split('|')[7]
 
-    puts "#{title} <#{'*' * rating[2].to_i}>"
+    if line.include? ARGV.join(' ')
+      found_title = "#{title} <#{'*' * rating[2].to_i}>"
+    end
+  end
+
+  if found_title != nil
+    puts found_title
+  else
+    puts 'Movie with this titls not found'
   end
 end
