aaac370ae7bfb56fb7bd27dd6d5431ce6b2ce036
vadis <vadis.programmist@mail.ru>
2016-12-21 23:27:37 +0300

    Fixed checking for ARGV

--- file_reader.rb
+++ file_reader.rb
@@ -1,14 +1,24 @@
 #encoding: UTF-8
 
-file_path = File.dirname(__FILE__) + "./lesson2/#{ARGV.join}"
-
-if !File.exist?(file_path) || ARGV.empty?
-  file_path = File.dirname(__FILE__) + './lesson2/movies.txt'
+if !ARGV.empty?
+  file_path = File.dirname(__FILE__) + "./lesson2/#{ARGV.join}"
+else
+  file_path = File.dirname(__FILE__) + "./lesson2/movies.txt"
 end
 
-File.open(file_path) do |file|
-  file.readlines.each do |line|
-    info_movie = line.split('|')
-    puts "#{info_movie[1]} <#{'*' * info_movie[7][2].to_i}>"
+if File.exist? file_path
+  File.open(file_path) do |file|
+    file.readlines.each do |line|
+      info_movie = line.split('|')
+      rating = info_movie[7]
+            
+      if rating[1].to_i < 9
+        print "#{info_movie[1]} <#{'*' * rating[2].to_i}> "
+      else
+        print "#{info_movie[1]} <#{'*' * rating[2].to_i * 10}> "
+      end
+    end
   end
+else
+  puts "file not found"
 end
