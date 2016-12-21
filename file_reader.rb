2feeb893c0a0370c63e264d2370530d5eae5b688
vadis <vadis.programmist@mail.ru>
2016-12-21 03:14:16 +0300

    Path as argumeent time to start program

--- file_reader.rb
+++ file_reader.rb
@@ -1,8 +1,11 @@
 #encoding: UTF-8
 
+if ARGV.empty?
+  puts "U didn't write anything as argument"
+  exit
+end
 
-
-path = File.dirname(__FILE__) + './lesson2/movies.txt'
+path = File.dirname(__FILE__) + ARGV.join
 
 if File.exist? path
   puts 'file exist now U can process the file'
@@ -13,20 +16,10 @@ end
 
 File.open(path) do |file|
 
-  found_title = nil
-
   file.readlines.each do |line|
     title = line.split('|')[1]
     rating = line.split('|')[7]
 
-    if line.include? ARGV.join(' ')
-      found_title = "#{title} <#{'*' * rating[2].to_i}>"
-    end
-  end
-
-  if found_title != nil
-    puts found_title
-  else
-    puts 'Movie with this titls not found'
+    puts "#{title} <#{'*' * rating[2].to_i}>"
   end
 end
