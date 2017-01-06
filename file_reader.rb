67bc4b18590bd8a7ab2f949851aa4319eb144f1f
vadis <vadis.programmist@mail.ru>
2017-01-06 14:55:33 +0300

    Saved date in Hashes

--- file_reader.rb
+++ file_reader.rb
@@ -1,16 +1,16 @@
 #encoding: UTF-8
 
-file_path = File.dirname(__FILE__) + "./lesson2/" + (ARGV.first || '/movies.txt')
+path = File.dirname(__FILE__) + './movies/' + (ARGV.first || 'movies.txt')
 
-if !File.exist? file_path
-  puts "file not found"
+if !File.exist? path
+  puts 'File not found'
   exit
 end
 
-File.open(file_path) do |file|
-  file.readlines.each do |line|
-    title, rating = line.split('|').values_at(1, 7)
-    stars = (rating.to_f - 8).round(1) * 10
-    print "#{title} #{'*' * stars} "
-  end
-end
+file = File.new path
+
+keys = %i(link title year country release genre time rating director stars)
+
+file.readlines.map{|line| keys.zip(line.split('|')).to_h}
+
+file.close
