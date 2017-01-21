42ef997f64595c9b5db55ac9213e6e8edb5b15f7
vadis <vadis.programmist@mail.ru>
2017-01-21 22:06:45 +0300

    added own method

--- file_reader.rb
+++ file_reader.rb
@@ -14,25 +14,11 @@ keys = %i(link title year country release genre time rating director stars)
 movies = file.readlines.map{|line| keys.zip(line.split('|')).to_h}
 file.close
 
-long_movies = movies.sort_by{|movie| movie[:time].to_i}.last(5)
-#показать 5 самых длынных фильмов
-puts long_movies.map{|movie| movie[:title]}
+def show_movies(movies)
+  movies.map do |movie|
+    "#{movie[:title]} (#{movie[:release]}; " +
+    "#{movie[:genre].gsub(' ', '/')}) - #{movie[:time]}".join("/n")
+  end
+end
 
-puts
-
-#10 комедий (первые по дате выхода)
-puts movies.
-  find_all{|movie| movie[:genre].include? 'Comedy'}.
-  sort_by{|movie| movie[:release]}.map{|movie| movie[:title]}.first(10)
-
-puts
-
-#список всех режиссёров по алфавиту (без повторов!) — по фамилии
-puts movies.
-  map{|movie| movie[:director]}.uniq.
-  sort_by{|director| director.split(' ').last}
-
-puts
-
-#количество фильмов, снятых не в США
-puts movies.count{|movie| movie[:country] != 'USA'}
+puts show_movies(movies)
