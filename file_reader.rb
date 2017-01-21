ce4b13050a54312f3a7adfe0da8d9ab9d57bc8a9
vadis <vadis.programmist@mail.ru>
2017-01-22 02:53:21 +0300

    added own method in task with longest movies and comedies

--- file_reader.rb
+++ file_reader.rb
@@ -1,5 +1,12 @@
 #encoding: UTF-8
 
+def show_movies(movies)
+  movies.map do |movie|
+    "#{movie[:title]} (#{movie[:release]}; " +
+    "#{movie[:genre].gsub(' ', '/')}) - #{movie[:time]}"
+  end.join("\n")
+end
+
 path = File.dirname(__FILE__) + './movies/' + (ARGV.first || 'movies.txt')
 
 if !File.exist? path
@@ -11,37 +18,24 @@ file = File.new path
 
 keys = %i(link title year country release genre time rating director stars)
 
-movies = file.readlines.map{|line| keys.zip(line.split('|')).to_h}
-file.close
-
-long_movies = movies.sort_by{|movie| movie[:time].to_i}.last(5)
+movies = File.new(path).readlines.map{|line| keys.zip(line.split('|')).to_h}
 #показать 5 самых длынных фильмов
-puts long_movies.map{|movie| movie[:title]}
+puts show_movies(movies.sort_by{|movie| movie[:time].to_i}.last(5))
 
 puts
-
 #10 комедий (первые по дате выхода)
-puts movies.
-  find_all{|movie| movie[:genre].include? 'Comedy'}.
-  sort_by{|movie| movie[:release]}.map{|movie| movie[:title]}.first(10)
+puts show_movies(
+  movies.
+    find_all{|movie| movie[:genre].include? 'Comedy'}.
+    sort_by{|movie| movie[:release]}.first(10)
+)
 
 puts
-
 #список всех режиссёров по алфавиту (без повторов!) — по фамилии
 puts movies.
   map{|movie| movie[:director]}.uniq.
   sort_by{|director| director.split(' ').last}
 
 puts
-
 #количество фильмов, снятых не в США
 puts movies.count{|movie| movie[:country] != 'USA'}
-
-def show_movies(movies)
-  movies.map do |movie|
-    "#{movie[:title]} (#{movie[:release]}; " +
-    "#{movie[:genre].gsub(' ', '/')}) - #{movie[:time]}".join("/n")
-  end
-end
-
-puts show_movies(movies)
