9cfdb2b174857ed0907d069a3548255fb5b8c2d2
Vadis <vadis.programmist@gmail.com>
2017-02-11 19:07:38 +0300

    added parsing with CSV library

--- file_reader.rb
+++ file_reader.rb
@@ -1,41 +1,12 @@
 #encoding: UTF-8
+require 'csv'
 
-def show_movies(movies)
-  movies.map do |movie|
-    "#{movie[:title]} (#{movie[:release]}; " +
-    "#{movie[:genre].gsub(' ', '/')}) - #{movie[:time]}"
-  end.join("\n")
-end
+abort 'File not found' unless File.exist?('./movies.txt')
 
-path = File.dirname(__FILE__) + './movies/' + (ARGV.first || 'movies.txt')
+HEADER = %w(link title year country release genre time rating director stars)
 
-if !File.exist? path
-  puts 'File not found'
-  exit
-end
-
-file = File.new path
-
-keys = %i(link title year country release genre time rating director stars)
-
-movies = File.new(path).readlines.map{|line| keys.zip(line.split('|')).to_h}
-#показать 5 самых длынных фильмов
-puts show_movies(movies.sort_by{|movie| movie[:time].to_i}.last(5))
-
-puts
-#10 комедий (первые по дате выхода)
-puts show_movies(
-  movies.
-    find_all{|movie| movie[:genre].include? 'Comedy'}.
-    sort_by{|movie| movie[:release]}.first(10)
+movies = CSV.read(
+  './movies.txt',
+  col_sep: '|',
+  headers: HEADER,
 )
-
-puts
-#список всех режиссёров по алфавиту (без повторов!) — по фамилии
-puts movies.
-  map{|movie| movie[:director]}.uniq.
-  sort_by{|director| director.split(' ').last}
-
-puts
-#количество фильмов, снятых не в США
-puts movies.count{|movie| movie[:country] != 'USA'}
