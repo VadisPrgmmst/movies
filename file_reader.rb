2025c30f353f846e840b569118c57c42c2d5a111
Vadis <vadis.programmist@gmail.com>
2017-02-11 19:17:51 +0300

    data is kept in OpenStruct

--- file_reader.rb
+++ file_reader.rb
@@ -1,5 +1,13 @@
 #encoding: UTF-8
 require 'csv'
+require 'ostruct'
+
+def show_movies(movies)
+  movies.map{ |m|
+    m.title + " (" + m.release + "; " +
+    m.genre.gsub(',','/') + ") - " + m.time
+  }.join("\n")
+end
 
 abort 'File not found' unless File.exist?('./movies.txt')
 
@@ -9,4 +17,18 @@ movies = CSV.read(
   './movies.txt',
   col_sep: '|',
   headers: HEADER,
+).map{|row|OpenStruct.new(row.to_h)}
+
+puts show_movies(movies.select{|m| m.time.to_i}.last(5))
+
+puts
+puts show_movies(
+  movies.select{|m| m.genre.include?('Comedy')}.
+    sort_by(&:release).first(10)
 )
+
+puts
+puts movies.map(&:director).uniq.sort_by{|m| m.split(' ').last}
+
+puts
+puts movies.count{|m| m.country != 'USA'}
